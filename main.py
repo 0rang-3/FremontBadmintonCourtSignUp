fe04fd3f16361256f5fcb7c77faf6fb516f6c496
Ruhan Pandit <ruhanpandit@Mac-328529.local>
2023-11-14 03:11:24 -0800

    remove ap scheduler

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request
-from apscheduler.schedulers.background import BackgroundScheduler
 import time, atexit
 from datetime import datetime
 
